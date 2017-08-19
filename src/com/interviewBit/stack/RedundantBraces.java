f48a6ad11135ff5a59754d49a1ec1b8947a526ce
Rajeev Krishna Singh <rajeevkri@gmail.com>
2017-08-19 20:46:29 +0530

    Redundant braces

---
+++ src/com/interviewBit/stack/RedundantBraces.java
@@ -0,0 +1,50 @@
+package com.interviewBit.stack;
+
+import java.util.Stack;
+
+/**
+ * Write a program to validate if the input string has redundant braces? Return
+ * 0/1
+ * 
+ * 0 -> NO 1 -> YES
+ * 
+ * Input will be always a valid expression
+ * 
+ * and operators allowed are only + , * , - , /
+ * 
+ * Example:
+ * 
+ * ((a + b)) has redundant braces so answer will be 1 (a + (a + b)) doesn't have
+ * have any redundant braces so answer will be 0
+ * 
+ * @author rajeevsingh
+ *
+ */
+public class RedundantBraces {
+	public static int braces(String a) {
+		Stack<Character> stack = new Stack<Character>();
+		int index = 0;
+		while (index < a.length()) {
+			char c = a.charAt(index);
+			if (c == '(' || c == '+' || c == '-' || c == '*' || c == '/') {
+				stack.push(c);
+			} else if (c == ')') {
+				if (stack.peek() == '(') {
+					return 1;
+				} else {
+					while (!stack.isEmpty() && stack.peek() != '(') {
+						stack.pop();
+					}
+					stack.pop();
+				}
+			}
+			index++;
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		String a = "(a)";
+		System.out.println(braces(a));
+	}
+}
