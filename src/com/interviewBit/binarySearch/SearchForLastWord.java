1d1df488229fed2cfff09e1e23aa34a80efe642d
Rajeev Krishna Singh <rajeevkri@gmail.com>
2017-08-29 20:33:45 +0530

    Search for last word

---
+++ src/com/interviewBit/binarySearch/SearchForLastWord.java
@@ -0,0 +1,19 @@
+package com.interviewBit.binarySearch;
+
+public class SearchForLastWord {
+	public static int lengthOfLastWord(String a) {
+		if(a== null || a.length() == 0) {
+			return 0;
+		}
+		a = a.trim();
+		int l = a.lastIndexOf(" ");
+		if(l < 0) {
+			return a.length();
+		}
+		return a.length() - l -1;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(lengthOfLastWord("Hello world"));
+	}
+}
